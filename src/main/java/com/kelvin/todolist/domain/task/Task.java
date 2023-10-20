45b0e090b180e2d65f556fe6669f2822202a4552
Kelvin <kelvinmarquesk@gmail.com>
2023-10-20 11:58:57 -0300

    Add class Task

---
+++ src/main/java/com/kelvin/todolist/domain/task/Task.java
@@ -0,0 +1,33 @@
+package com.kelvin.todolist.domain.task;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(of = "id")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private boolean completed;
+}
