8b49cb1628c6e87467b5df0dc584c9512056404e
Kelvin <kelvinmarquesk@gmail.com>
2023-10-21 21:22:35 -0300

    Add class TaskController

---
+++ src/main/java/com/kelvin/todolist/controller/TaskController.java
@@ -0,0 +1,57 @@
+package com.kelvin.todolist.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.kelvin.todolist.domain.task.Task;
+import com.kelvin.todolist.repository.TaskRepository;
+
+import jakarta.transaction.Transactional;
+
+@RestController
+@RequestMapping("tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository repository;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity cadastrar(@RequestBody @Valid Task task, UriComponentsBuilder uriBuilder) {
+        Task newTask = repository.save(task);
+        var uri = uriBuilder.path("/tasks/{id}").buildAndExpand(newTask.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping
+    public ResponseEntity listar(Pageable paginacao) {
+        var page = repository.findAll(paginacao).stream().map(TaskResponseDTO::new);
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> detalhar(@PathVariable Long id) {
+        var task = repository.findById(id);
+        if (task.isPresent()) {
+            return ResponseEntity.ok(task.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity excluir(@PathVariable Long id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
