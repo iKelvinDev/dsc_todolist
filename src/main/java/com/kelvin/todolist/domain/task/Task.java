eb408da59446fd77e006e2c091fbe021bc677ab6
kelvin <kelvinmarquesk@gmail.com>
2023-11-01 01:19:52 -0300

    Add relationship ManyToOne for Task and User

--- src/main/java/com/kelvin/todolist/domain/task/Task.java
+++ src/main/java/com/kelvin/todolist/domain/task/Task.java
@@ -3,11 +3,14 @@ package com.kelvin.todolist.domain.task;
 import java.time.LocalDate;
 
 import com.kelvin.todolist.domain.enumeration.TaskPriority;
+import com.kelvin.todolist.domain.user.User;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -16,8 +19,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Table
-@Entity
+@Table(name = "task")
+@Entity(name = "task")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -25,13 +28,27 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode(of = "id")
 public class Task {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    //@NotBlank(message = "Task title is required")
     private String title;
+
     private String description;
+
     private LocalDate startDate = LocalDate.now();
+
+    //@NotBlank(message = "Task due date is required")
     private LocalDate dueDate;
+
+    //@NotBlank(message = "Task priority is required")
     private TaskPriority priority;
+
     private boolean completed;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
